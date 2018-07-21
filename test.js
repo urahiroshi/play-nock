17f6f6501829723d6f23dcc5e78a8532c7875eb2
urahiroshi <asj.hiroshi@gmail.com>
2018-07-21 16:16:26 +0900

    Filter for timestamp

--- test.js
+++ test.js
@@ -1,11 +1,25 @@
+
 const axios = require('axios');
 const nockBack = require('nock').back;
 
 // nockBack.setMode('record');
 nockBack.fixtures = __dirname + '/nockFixtures';
 
+const nockBackOptions = {
+  before: function (scope) {
+    scope.filteringRequestBody = function (body, aRecordedBody) {
+      const fixedBody = JSON.parse(body);
+      if (aRecordedBody.timestamp && fixedBody.timestamp) {
+        fixedBody.timestamp = aRecordedBody.timestamp;
+        return JSON.stringify(fixedBody);
+      }
+      return body;
+    }
+  },
+};
+
 const test = async () => {
-  const { nockDone } = await nockBack('httpbin.json');
+  const { nockDone, context } = await nockBack('httpbin.json', nockBackOptions);
   const res = await axios.post('http://httpbin.org/post', { timestamp: (new Date()).toString() });
   console.log(res.data);
   nockDone();
